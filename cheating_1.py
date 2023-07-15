e578f23160dd609e1089e796ea09b2148347d31a
CyuuZ Chan <124024371+cyuuz-chan@users.noreply.github.com>
2023-07-15 19:16:00 +0800

    修复结果中包含开出来字母没开出来的

--- cheating_1.py
+++ cheating_1.py
@@ -18,8 +18,15 @@ def find_matching_packages(zxgaer_aishiteru_a, zxgaer_aishiteru_b):
                     zxgaer_aishiteru_e = False
                     break
             if zxgaer_aishiteru_e:
-                zxgaer_aishiteru_c.append(zxgaer_aishiteru_d)
+                # 过滤结果中开出来字母没开出来的
+                for char in set(zxgaer_aishiteru_b):
+                    if char != '*' and zxgaer_aishiteru_b.count(char) < zxgaer_aishiteru_d.count(char):
+                        zxgaer_aishiteru_e = False
+                        break
+                if zxgaer_aishiteru_e:
+                    zxgaer_aishiteru_c.append(zxgaer_aishiteru_d)
     return zxgaer_aishiteru_c
+
 
 # 用命令获取所有包，前提是系统为ArchLinux
 zxgaer_aishiteru_f = os.popen('pacman -Ss | grep -oP "^[\w-]+/[\w-]+" | cut -d/ -f2').read().split('\n')
@@ -27,11 +34,11 @@ zxgaer_aishiteru_f = os.popen('pacman -Ss | grep -oP "^[\w-]+/[\w-]+" | cut -d/ 
 try:
     while True:
         # 获取用户输入内容
-        zxgaer_aishiteru_g = input('Enter a package name with *:')
+        zxgaer_aishiteru_g = input('请您输入捏:')
         # 查找匹配的软件包
         zxgaer_aishiteru_h = find_matching_packages(zxgaer_aishiteru_f, zxgaer_aishiteru_g)
         # 输出结果
         print(zxgaer_aishiteru_h)
 except KeyboardInterrupt:
     # 退出
-    print('\nGoodbye')
+    print('\n拜拜')
